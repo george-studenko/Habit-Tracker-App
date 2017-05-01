2d97252faf511f482b10562a64ce7186b1c7585b
George Studenko <gnstudenko@gmail.com>
2017-05-01 22:14:04 +0200

    feat: Add HabitDbHelper to Main Activity

--- app/src/main/java/com/georgestudenko/habittracker/MainActivity.java
+++ app/src/main/java/com/georgestudenko/habittracker/MainActivity.java
@@ -1,9 +1,12 @@
 package com.georgestudenko.habittracker;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.facebook.stetho.Stetho;
+import com.georgestudenko.habittracker.data.HabitDbHelper;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Stetho.initializeWithDefaults(this);
+
+        HabitDbHelper dbHelper = new HabitDbHelper(this,HabitDbHelper.DATABASE_NAME,null,HabitDbHelper.DATABASE_VERSION);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
     }
 }
